d0f6e6eb623804794c16b89e0fcbde593376ed0c
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2024-09-26 08:58:40 +0200

    Visualize layouts

--- src/cuda_prototype/swizzle-test.cu
+++ src/cuda_prototype/swizzle-test.cu
@@ -6,12 +6,22 @@ int main()
 {
     using namespace cute;
     
-    auto bM = Int<8>{};
+    auto bM = Int<16>{};
     auto bK = Int<64>{};
-    auto sA = make_layout(make_shape(bM, bK), make_stride(Int<8>{}, Int<1>{}));
+    auto sA = make_layout(make_shape(Int<8>{}, Int<8>{}), 
+                          make_stride(Int<8>{}, Int<1>{}));
     
-    auto sA_swizzled = composition(Swizzle<2,3,3>{}, sA);
+    // auto sA_swizzled = composition(Swizzle<3,0>{}, sA);
+    // TiledCopy copyA = make_tiled_copy(Copy_Atom<UniversalCopy<uint128_t>, half_t>{},
+    //                                   Layout<Shape<_16, _8>, Stride<_8, _1>>{},
+    //                                   Layout<Shape<_1, _8>>{});
     
+    using value_type = cute::half_t;
+    auto smem_layout = composition(Swizzle<3,3,3>{},
+                Layout<Shape < _8,_64>,
+                       Stride<_64, _1>>{});
+    // 0, 0 -> 0
+    // 1 -> 0
     // print_layout(sA);
-    print_latex(sA_swizzled);
+    print_latex(smem_layout);
 }
